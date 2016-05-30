52e43a8957a998859cc66d40bd926faf741e5282
gogakoreli@gmail.com <gogakoreli@gmail.com>
2016-05-30 23:37:23 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ExamAssistant
+Final OOP Project
+
+Apaches me8 versia tu ar gaqvt gadmoweret da daayenet : http://www-us.apache.org/dist/tomcat/tomcat-8/v8.0.35/bin/apache-tomcat-8.0.35.exe
---
+++ src/Servlets/LoginServlet.java
@@ -0,0 +1,44 @@
+package Servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public LoginServlet()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.getOutputStream().println("<html>  <body> Mushaobs </body> </html>"); 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+	}
+
+}
