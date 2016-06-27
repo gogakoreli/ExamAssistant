5193d58050e78b2ed6c329f90f76f8fb425f113b
giorgigona <ggona14@freeuni.edu.ge>
2016-06-27 14:33:07 +0400

    chveulebrivi sesiidan httpSession is amogeba daemata.
    aseve daemata axai klasi ServletAwareConfig.

--- src/chat_server/ChatroomServerEndpoint.java
+++ src/chat_server/ChatroomServerEndpoint.java
@@ -17,12 +17,14 @@ import javax.websocket.server.ServerEndpoint;
 
 import com.google.gson.Gson;
 
+import data_managers.AccountManager;
+import models.EAUser;
+
 @ServerEndpoint(value = "/ChatroomServerEndpoint", configurator = ServletAwareConfig.class)
 public class ChatroomServerEndpoint {
+	
 	private Set<Session> chatroomUsers;
-	private Session wsSession;
-    private HttpSession httpSession;
-
+	private HttpSession httpSession;
 
 	public ChatroomServerEndpoint() {
 		chatroomUsers = Collections.synchronizedSet(new HashSet<Session>());
@@ -30,11 +32,12 @@ public class ChatroomServerEndpoint {
 
 	@OnOpen
 	public void onOpen(Session userSession, EndpointConfig config) {
-		 this.wsSession = userSession;
-	     this.httpSession = (HttpSession) config.getUserProperties()
-	                                           .get(HttpSession.class.getName());
-	     System.out.println(httpSession.getId());
-		//chatroomUsers.add(userSession);
+		httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
+		chatroomUsers.add(userSession);
+		Iterator<Session> it = chatroomUsers.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next().getId());
+		}
 	}
 
 	@OnClose
@@ -42,43 +45,43 @@ public class ChatroomServerEndpoint {
 		chatroomUsers.remove(userSession);
 	}
 
-	/*
 	@OnMessage
-	public void onMessage(Session userSession, String message) throws IOException {
-		
-		//HttpSession httpSession = (HttpSession) config.getUserProperties().get("httpSession");
-		//HttpSession httpSession1 = (HttpSession)userSession.getUserProperties().get("httpSession");
-		
-		HttpSession httpSession2 = (HttpSession) config.getUserProperties()
-                .get(HttpSession.class.getName());
-		userName = "giorgi";
-       // ServletContext servletContext = httpSession.getServletContext();
-        //System.out.println(httpSession.getId());
-        //System.out.println(httpSession1.getId());
-        System.out.println(httpSession2.getId());
-		String userName = (String) userSession.getUserProperties().get("userName");
-		if (userName != null) {
-			userSession.getUserProperties().put("userName", message);
-			userSession.getBasicRemote().sendText(buildJson("System", "Your message is " + message));
-		} else {
-			Iterator<Session> it = chatroomUsers.iterator();
-			while (it.hasNext())
-				it.next().getBasicRemote().sendText(buildJson(userName, message));
+	public void onMessage(String msg, Session wsSession) throws IOException {
+		AccountManager accountManager = AccountManager.getAccountManager(httpSession);
+		EAUser user = accountManager.getCurrentUser(httpSession);
+		//wsSession.getBasicRemote().sendText(user.getFirstName() + " :" + msg + httpSession.getId());
+		Iterator<Session> it = chatroomUsers.iterator();
+		while (it.hasNext()) {
+			it.next().getBasicRemote().sendText(buildJson(user.getFirstName(), msg + httpSession.getId()));
+			
 		}
 	}
-	*/
-	
-	
-	 @OnMessage
-	    public void echo(String msg) throws IOException {
-	        wsSession.getBasicRemote().sendText("1");
-	        System.out.println(httpSession.getId());
-	    }
-	    
-/*
+
+	/*
+	 * @OnMessage public void onMessage(Session userSession, String message)
+	 * throws IOException {
+	 * 
+	 * //HttpSession httpSession = (HttpSession)
+	 * config.getUserProperties().get("httpSession"); //HttpSession httpSession1
+	 * = (HttpSession)userSession.getUserProperties().get("httpSession");
+	 * 
+	 * HttpSession httpSession2 = (HttpSession) config.getUserProperties()
+	 * .get(HttpSession.class.getName()); userName = "giorgi"; // ServletContext
+	 * servletContext = httpSession.getServletContext();
+	 * //System.out.println(httpSession.getId());
+	 * //System.out.println(httpSession1.getId());
+	 * System.out.println(httpSession2.getId()); String userName = (String)
+	 * userSession.getUserProperties().get("userName"); if (userName != null) {
+	 * userSession.getUserProperties().put("userName", message);
+	 * userSession.getBasicRemote().sendText(buildJson("System",
+	 * "Your message is " + message)); } else { Iterator<Session> it =
+	 * chatroomUsers.iterator(); while (it.hasNext())
+	 * it.next().getBasicRemote().sendText(buildJson(userName, message)); } }
+	 */
+
 	private String buildJson(String userName, String message) {
 		String json = new Gson().toJson(userName + " :" + message);
 		return json;
 	}
-*/
+
 }
--- src/chat_server/ServletAwareConfig.java
+++ src/chat_server/ServletAwareConfig.java
@@ -7,11 +7,9 @@ import javax.websocket.server.ServerEndpointConfig;
 
 public class ServletAwareConfig extends ServerEndpointConfig.Configurator {
 
-	private HttpSession httpSession;
 	
     @Override
     public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
-    	//super.modifyHandshake(config, request, response);
         HttpSession httpSession = (HttpSession) request.getHttpSession();
         config.getUserProperties().put(HttpSession.class.getName(), httpSession);
     }
