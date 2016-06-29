0fb60d8029c5504801ed4b989baafc0905fc4ac8
giorgigona <ggona14@freeuni.edu.ge>
2016-06-30 00:22:05 +0400

    Daemata Notification Serveri romlis onMessage gasamartia.
    aseve daemata LecturerNotification.html romelic shesacvlelia

--- src/helper/DBInfo.java
+++ src/helper/DBInfo.java
@@ -11,7 +11,7 @@ public class DBInfo {
 	
 	//offline database
 	public static final String MYSQL_USERNAME = "root";
-	public static final String MYSQL_PASSWORD = "root";
+	public static final String MYSQL_PASSWORD = "Gonagona";
 	public static final String MYSQL_DATABASE_SERVER = "localhost:3306";
 	public static final String MYSQL_DATABASE_NAME = "examassistant";
 	
--- src/notification_server/NotificationServerEndpoint.java
+++ src/notification_server/NotificationServerEndpoint.java
@@ -1,11 +1,11 @@
 package notification_server;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.http.HttpSession;
 import javax.websocket.EndpointConfig;
@@ -15,39 +15,98 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/NotificationServerEndpoint")
+import data_managers.AccountManager;
+import data_managers.ExamManager;
+import models.EAUser;
+import models.Lecturer;
+import models.Student;
+
+
+@ServerEndpoint(value="/NotificationServerEndpoint", configurator = NotificationServletAwareConfig.class)
 public class NotificationServerEndpoint {
 
-	private Set<Session> chatroomUsers;
+	private static final Map<Session, HttpSession> sessions = Collections
+			.synchronizedMap(new HashMap<Session, HttpSession>());
 
 	@OnOpen
-	public void onOpen(Session userSession) {
-		System.out.println("Opened: " + userSession.getId());
-		//chatroomUsers.add(userSession);
-		System.out.println("Opened2");
+	public void onOpen(Session userSession, EndpointConfig config) {
+		HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
+		sessions.put(userSession, httpSession);
+		System.out.println("Opened"+userSession.getId()+" "+httpSession.getId());
 	}
 
 	@OnClose
 	public void onClose(Session userSession) {
-		System.out.println("Closed1");
-		//chatroomUsers.remove(userSession);
-		System.out.println("Closed2");
+		sessions.remove(userSession);
+		System.out.println("Closed"+userSession.getId());
 	}
 
 	@OnMessage
-	public void onMessage(Session userSession, String message) throws IOException {
-
-		String userName = (String) userSession.getUserProperties().get("userName");
-		userName = "giorgi";
-		if (userName != null) {
-			userSession.getUserProperties().put("userName", message);
-			userSession.getBasicRemote().sendText(message);
-		} else {
-			Iterator<Session> it = chatroomUsers.iterator();
-			while (it.hasNext())
-				it.next().getBasicRemote().sendText(message + " :" + userSession.getId());
+	public void onMessage(String gsonMessage, Session userSession) throws IOException {
+		System.out.println(gsonMessage+" "+userSession.getId());
+		
+		
+		if(!sessionIsLecturer(userSession)){
+			System.out.println("user is not lecturer");
 		}
+		
+		HttpSession httpSession = sessions.get(userSession);
+		AccountManager accountManager = AccountManager.getAccountManager(httpSession);
+		
+		Lecturer lecturer = (Lecturer) accountManager.getCurrentUser(httpSession);
+		System.out.println(lecturer.getFirstName());
+		
+		Iterator<Session> it = sessions.keySet().iterator();
+		while (it.hasNext()) {
+			Session curSession = it.next();
+			if (curSession.isOpen()) {
+				curSession.getBasicRemote().sendText(gsonMessage);
+			}
+		}
+		
 
+	}
+	
+	public class NotiMessageForLecturer {
+		public String message;
+		public int examId;
+		public ArrayList<Integer> variants;
+		
+		public NotiMessageForLecturer(String message, int examId, ArrayList<Integer> variants) {
+			this.message = message;
+			this.examId = examId;
+			this.variants = new ArrayList<Integer>(variants);
+		}
+	}
+	
+	public class NotiMessageForStudent{
+		public String message;
+		public String name;
+		
+		public NotiMessageForStudent(String message, String name){
+			this.message = message;
+			this.name = name;
+		}
+	}
+	
+	/**
+	 * returns appropriate EAUser to session
+	 * 
+	 * @param session
+	 * @return
+	 */
+	private EAUser getUserFromSession(Session session) {
+		HttpSession httpSession = sessions.get(session);
+		AccountManager accountManager = AccountManager.getAccountManager(httpSession);
+		return accountManager.getCurrentUser(httpSession);
+	}
+
+	private boolean sessionIsStudent(Session session) {
+		return getUserFromSession(session) instanceof Student;
+	}
+
+	private boolean sessionIsLecturer(Session session) {
+		return getUserFromSession(session) instanceof Lecturer;
 	}
 
 }
