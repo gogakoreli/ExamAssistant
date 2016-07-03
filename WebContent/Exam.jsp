38f7ea7d8b0df7dd07933ff1e43948c423586372
gogakoreli <gogakoreli@gmail.com>
2016-07-03 14:12:31 +0400

    Uploads added

--- WebContent/Exam.jsp
+++ WebContent/Exam.jsp
@@ -1,4 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=utf-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
 <%@ page import="java.util.*, models.*, helper.*"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -77,15 +78,30 @@ h1 {
 			<span class="hours"></span>
 			<div class="smalltext">საათი</div>
 		</div>
+		
 		<div>
 			<span class="minutes"></span>
 			<div class="smalltext">წუთი</div>
 		</div>
+		
 		<div>
 			<span class="seconds"></span>
 			<div class="smalltext">წამი</div>
 		</div>
 	</div>
+	
+	<br>
+	<br>
+	
+	 <form method="POST" action="Upload" enctype="multipart/form-data" >
+            File:
+            <input type="file" name="file" id="file" />
+            <br>
+            Destination:
+            <input type="text" value="/tmp" name="destination"/>
+            <br>
+            <input type="submit" value="Upload" name="upload" id="upload" />
+       </form>
 
 	<script type="text/javascript">
 		function getTimeRemaining(endtime) {
---
+++ src/servlets/UploadServlet.java
@@ -0,0 +1,144 @@
+package servlets;
+
+import java.io.File;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.*;
+import java.util.*;
+ 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.output.*;
+
+/**
+ * Servlet implementation class UploadServlet
+ */
+@WebServlet("/Upload")
+public class UploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private boolean isMultipart;
+	private String filePath;
+	private int maxFileSize = 500 * 1024;
+	private int maxMemSize = 100 * 1024;
+	private File file;
+
+	public void init() {
+		// Get the file location where it would be stored.
+		filePath = getServletContext().getInitParameter("file-upload");
+	}
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UploadServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Check that we have a file upload request
+	      isMultipart = ServletFileUpload.isMultipartContent(request);
+	      response.setContentType("text/html");
+	      PrintWriter out = response.getWriter( );
+	      
+	      
+	      if( !isMultipart ){
+	    	  response.setContentType("text/html");  
+		      out.println("<script type=\"text/javascript\">");  
+		      out.println("alert('ERROR\nNo File Uploaded');");  
+		      out.println("</script>");
+	         
+	         
+	         return;
+	      }
+	      
+	      DiskFileItemFactory factory = new DiskFileItemFactory();
+	      // maximum size that will be stored in memory
+	      factory.setSizeThreshold(maxMemSize);
+	      // Location to save data that is larger than maxMemSize.
+	      factory.setRepository(new File("c:\\temp"));
+
+	      // Create a new file upload handler
+	      ServletFileUpload upload = new ServletFileUpload(factory);
+	      // maximum file size to be uploaded.
+	      upload.setSizeMax( maxFileSize );
+	      
+	      // set upload encoding
+	      upload.setHeaderEncoding("UTF-8"); 
+
+	      try{ 
+	      // Parse the request to get file items.
+	      List fileItems = upload.parseRequest(request);
+		
+	      // Process the uploaded file items
+	      Iterator i = fileItems.iterator();
+
+	      out.println("<html>");
+	      out.println("<head>");
+	      out.println("<title>Servlet upload</title>");  
+	      out.println("</head>");
+	      out.println("<body>");
+	      while ( i.hasNext () ) 
+	      {
+	         FileItem fi = (FileItem)i.next();
+	         if ( !fi.isFormField () )	
+	         {
+	            // Get the uploaded file parameters
+	            String fieldName = fi.getFieldName();
+	            String fileName = fi.getName();
+	            String contentType = fi.getContentType();
+	            boolean isInMemory = fi.isInMemory();
+	            long sizeInBytes = fi.getSize();
+	            // Write the file
+	            if( fileName.lastIndexOf("\\") >= 0 ){
+	               file = new File( filePath + 
+	               fileName.substring( fileName.lastIndexOf("\\"))) ;
+	            }else{
+	               file = new File( filePath + 
+	               fileName.substring(fileName.lastIndexOf("\\")+1)) ;
+	            }
+	            fi.write( file ) ;
+	            out.println("Uploaded Filename: " + fileName + "<br>");
+	         }
+	      }
+	      out.println("</body>");
+	      out.println("</html>");
+	   }catch(Exception ex) {
+	       System.out.println(ex);
+	   }
+	}
+
+}
+
+
+
+
