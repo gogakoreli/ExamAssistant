9066067529c1a6f4fb8331f50616251b0dc47674
AnaRakv <arakv14@freeuni.edu.ge>
2016-07-05 23:42:56 +0400

    Updated tests

--- src/models/Exam.java
+++ src/models/Exam.java
@@ -6,8 +6,6 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.media.jfxmedia.events.NewFrameEvent;
-
 import data_managers.ExamManager;
 import helper.LogManager;
 
